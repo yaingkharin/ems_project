586545570c52913fc8fdfdf481b08e6c53e3ba67
Rin_Kab_All <yaingkharin131@gmail.com>
2026-03-25 23:19:32 +0700

    Update booking URLs

--- app/urls/booking_urls.py
+++ app/urls/booking_urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from app.views.booking_views import BookingListCreateView, BookingRetrieveUpdateDestroyView, PaginatedBookingListView
+from app.views.booking_views import BookingListCreateView, BookingRetrieveUpdateDestroyView, PaginatedBookingListView, AdminBookingCreateView
 
 urlpatterns = [
     path('', BookingListCreateView.as_view(), name='booking-list-create'),
+    path('admin/', AdminBookingCreateView.as_view(), name='admin-booking-create'),
     path('<int:pk>/', BookingRetrieveUpdateDestroyView.as_view(), name='booking-retrieve-update-destroy'),
     path('paginate/', PaginatedBookingListView.as_view(), name='booking-paginate'),
 ]
